04105f70c59a30db708912fabc7a37c3de78264a
jimmy <jimmy@2DAM-003>
2025-11-07 14:00:45 +0100

    actualizado , corregido y test

--- ApplicationDEISignUpSignIn/test/applicationdeisignupsignin/ui/Sign_UpControllerTest.java
+++ ApplicationDEISignUpSignIn/test/applicationdeisignupsignin/ui/Sign_UpControllerTest.java
@@ -28,12 +28,12 @@ public void start(Stage stage) throws Exception{
  }
 
     @Test
-     public void test1_SomeMethod() {
+     public void test1_Focused() {
     verifyThat("#tfFirstName",isFocused()) ;
         
     }
    @Test
-    public void test2_InitialState() {
+    public void test2_Disabled() {
         clickOn("#tfEmail");
         write("use@nam.");
         verifyThat("#btSignUp", isDisabled());
@@ -45,7 +45,7 @@ public void start(Stage stage) throws Exception{
         verifyThat("#btSignUp", isDisabled());
     }
     @Test
-    public void test3_example(){
+    public void test3_SignUpErrorFrobidden(){
         clickOn("#tfFirstName");
         write("Jimmy");
         clickOn("#tfLastName");
@@ -70,6 +70,47 @@ public void start(Stage stage) throws Exception{
         write("28600");
         clickOn("#btSignUp");
         verifyThat("Error Email Registered", isVisible());
-        
+        clickOn("Aceptar");
+    }
+    @Test
+    public void test4_hySignInOpened(){
+    //Simula entrar en un enlace para comprobar que entra en otra ventana
+    clickOn("#hySignIn");
+    verifyThat("#btSignIn", isVisible());
+    }
+    @Test
+    public void test5_btBackOpened(){
+    //Simula pulsar el boton back para comprobar que entra en otra ventana
+    clickOn("#btBack");
+    verifyThat("#btSignIn", isVisible());
+    }
+    @Test
+    public void test6_SignInOpenClick() {
+    //Simula escribir datos correctos y comprueba que entra a otra ventana
+        clickOn("#tfFirstName");
+        write("Jimmy");
+        clickOn("#tfLastName");
+        write("Trasmonte");
+        clickOn("#tfMiddlelnitial");
+        write("J");
+        clickOn("#tfPhone");
+        write("640525968");
+        clickOn("#tfEmail");
+        write("CorreoPrueba10@gmail.com");//cambiar numero de correo para pruebas 
+        clickOn("#pfPassword");
+        write("contraseña");
+        clickOn("#pfConfirmPassword");
+        write("contraseña");
+        clickOn("#tfCity");
+        write("Madrid");
+        clickOn("#tfState");
+        write("Madrid");
+        clickOn("#tfStreet");
+        write("Calle, 21");
+        clickOn("#tfZip");
+        write("28600");
+        clickOn("#btSignUp");
+        clickOn("Aceptar");
+        verifyThat("#btSignIn", isVisible());
     }
 }
